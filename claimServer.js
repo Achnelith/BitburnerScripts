9caa9d39a246e2f79d7374bf1ee6491df8ee7b8f
Achnelith <kevin_dittman@outlook.com>
2023-03-09 18:18:49 -0500

    Reorder

--- claimServer.js
+++ claimServer.js
@@ -1,36 +1,54 @@
+  /**
+   * @param {NS} ns
+   * @param {string} target
+   **/
+  export async function main(ns) {
+    var targetName = ns.args[0];
+  
+    if (targetName === undefined) {
+      ns.tprint("Missing server name. Usage: `run claimServer.js n00dles`");
+      ns.exit();
+      return;
+    };
+  
+    let target = await fetchServer(ns, targetName);
+    let exes = await scanExes(ns);
+    root(ns, target, exes);
+  }
+
 /**
  * @param {NS} ns
- * @param {string} target
+ * @param {serverObj} targetObj
  * @param {string[]} exes
  **/
-export function root (ns, target, exes) {
-    if (target.hasAdminRights) {
+export function root (ns, targetObj, exes) {
+    if (targetObj.hasAdminRights) {
       ns.tprint("Already have root.");
       return;
     };
   
-    if ( target.portsRequired > exes.length) {
-      ns.tprint("Not enough tools to nuke " + target);
+    if ( targetObj.portsRequired > exes.length) {
+      ns.tprint("Not enough tools to nuke " + targetObj);
       return;
     };
   
     if (exes.includes("BruteSSH")) {
-      ns.brutessh(target.name);
+      ns.brutessh(targetObj.name);
     };
     if (exes.includes("FTPCrack")) {
-      ns.ftpcrack(target.name);
+      ns.ftpcrack(targetObj.name);
     };
     if (exes.includes("HTTPWorm")) {
-      ns.httpworm(target.name);
+      ns.httpworm(targetObj.name);
     };
     if (exes.includes("relaySMTP")) {
-      ns.relaysmtp(target.name);
+      ns.relaysmtp(targetObj.name);
     };
     if (exes.includes("SQLInject")) {
-      ns.sqlinject(target.name);
+      ns.sqlinject(targetObj.name);
     };
   
-    var ret = ns.nuke(target.name);
+    var ret = ns.nuke(targetObj.name);
     ns.tprint("Sudo aquired: " + ret);
   }
   
@@ -55,31 +73,13 @@ export function root (ns, target, exes) {
    * @param {NS} ns
    * @param {string} target
    **/
-  export async function fetchServer(ns, targetName) {
-      let sobj = {
+  async function fetchServer(ns, targetName) {
+      let serverObj = {
         name: targetName,
         portsRequired: ns.getServerNumPortsRequired(targetName),
-          hasAdminRights: ns.hasRootAccess(targetName)
+        hasAdminRights: ns.hasRootAccess(targetName)
         //hackingLvl: ns.getServerRequiredHackingLevel(server)
       };
   
-      return sobj;
-  }
-  
-  /**
-   * @param {NS} ns
-   * @param {string} target
-   **/
-  export async function main(ns) {
-    var targetName = ns.args[0];
-  
-    if (targetName === undefined) {
-      ns.tprint("Missing server name. Usage: `run claimServer.js n00dles`");
-      ns.exit();
-      return;
-    };
-  
-    let target = await fetchServer(ns, targetName);
-    let exes = await scanExes(ns);
-    root(ns, target, exes);
+      return serverObj;
   }
