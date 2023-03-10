dc985348e77e2262d292972e2a05455152523ecd
Achnelith <kevin_dittman@outlook.com>
2023-03-10 18:38:18 -0500

    Lazy init script

---
+++ greetNeighbors.js
@@ -0,0 +1,13 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  let neighbors = ["n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "hong-fang-tea", "harakiri-sushi", "iron-gym"];
+  ns.tprint("Starting  . . .");
+  neighbors.forEach(n => {
+    var pid = ns.run("claimServer.js", 1, n);
+     
+    if(pid == 0) {
+      ns.tprint("Failed to copy core scripts.");
+      return;
+    }
+  });
+}
